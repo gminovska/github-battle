af0b2b524d5ae0ba55fe4ab86337f475d6a6cb01
Gordana Minovska <ekogoca@gmail.com>
2017-05-30 15:27:05 -0700

    Configure webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+import path from 'path';
+
+module.exports = {
+    entry: './app/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                use: 'babel-loader'
+            },
+            {
+                test:/\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+
+        ]
+    }
+}
